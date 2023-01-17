6a67c7936a214d3ec7e125ef5e5f7d9534240b74
lamtota40 <lamtota40@gmail.com>
2023-01-17 17:02:59 +0700

    Update autovps2.sh

--- autovps2.sh
+++ autovps2.sh
@@ -10,7 +10,7 @@ sudo apt install grml-rescueboot
 cd /boot/grml
 sudo wget download.grml.org/grml64-full_2022.11.iso
 cd
-sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 vnc=pas123 toram netscript=bit.do/setup-ngrok\"' >> /etc/default/grml-rescueboot"
+sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 vnc=pas123 netscript=bit.do/setup-ngrok toram\"' >> /etc/default/grml-rescueboot"
 echo -ne '\n' | sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 echo -ne '\n' | sudo apt update
 sudo apt install grub-customizer -y
