ad8a3bdebaef48d858a9df6fd0a448cbfdcce50f
lamtota40 <lamtota40@gmail.com>
2023-01-22 09:34:11 +0700

    Update tes3.sh

--- tes3.sh
+++ tes3.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-useradd badak -d /home/badak -m ;
-echo ""badak":"qwerty"" | chpasswd
-echo bolopass | passwd bolo2;
-chsh -s /bin/bash badak
-echo "the account is compled"
-
+adduser gajah | echo -ne '\n' | echo "qwerty" | echo -ne '\n' | echo "qwerty"
