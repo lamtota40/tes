7b9e97879224f13c9bfa9b64fe2a0547a63a6a70
lamtota40 <lamtota40@gmail.com>
2023-01-22 09:15:32 +0700

    Update tes2.sh

--- tes2.sh
+++ tes2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-useradd kingkong -d /home/kingkong -m ;
-echo ""kingkonh":"qwerty"" | chpasswd
+useradd badak -d /home/badak -m ;
+echo ""badak":"qwerty"" | chpasswd
 echo bolopass | passwd bolo2;
-chsh -s /bin/bash kingkong
+chsh -s /bin/bash badak
 echo "the account is compled"
 
