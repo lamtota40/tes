c549ea143e02d99ba1a090573c089cafe4f7ae56
lamtota40 <lamtota40@gmail.com>
2023-01-29 18:41:36 +0700

    Update autovps2.sh

--- autovps2.sh
+++ autovps2.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
 echo "memulai"
-sudo apt update
-#sudo apt install lubuntu-core --no-install-recommends -y
+sudo -i
+adduser lubuntupc
+
+sudo apt update && sudo apt upgrade
 sudo apt install lubuntu-core -y
 sudo systemctl set-default graphical.target
 sudo apt install firefox -y
-sudo apt install gparted -y
-sudo apt install grml-rescueboot
-cd /boot/grml
-sudo wget download.grml.org/grml64-full_2022.11.iso
-cd
-mkdir /etc/grml/partconf
-cd /etc/grml/partconf
-sudo wget https://raw.githubusercontent.com/lamtota40/tes/main/setup-ngrok.sh
-cd
-sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 vnc=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/tes/main/setup-ngrok.sh startx toram\"' >> /etc/default/grml-rescueboot"
-echo -ne '\n' | sudo add-apt-repository ppa:danielrichter2007/grub-customizer
-echo -ne '\n' | sudo apt update
-sudo apt install grub-customizer -y
-sudo update-grub
-echo "====selesai====="
+
+wget https://github.com/robiot/xclicker/releases/download/v1.4.0/xclicker_1.4.0_amd64.deb -P /tmp
+sudo apt install /tmp/xclicker_1.4.0_amd64.deb -y
+sudo wget dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
+sudo apt install /tmp/chrome-remote-desktop_current_amd64.deb -y
+echo "selesai"
+sleep 15
 sudo reboot
 ## END OF SCRIPT #############################################
 
